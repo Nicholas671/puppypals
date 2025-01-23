65f0007f0688b9ada59dcecac92d9b017dbe721b
Nicholas671 <nicholas.a.crabtree@gmail.com>
2025-01-23 14:48:29 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { puppyList } from "./data"
+import React, { useState } from "react"
+
+function App() {
+
+  return (
+
+    console.log(puppyList),
+    <>
+      Puppy Pals
+    </>
+  )
+}
+
+export default App
