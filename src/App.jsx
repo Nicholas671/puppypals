ff5b07faa15c521da75e25a2f595b0e5180d35ad
Nicholas671 <nicholas.a.crabtree@gmail.com>
2025-01-24 10:39:35 -0700

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
-import { puppyList } from "./data"
-import React, { useState } from "react"
+import { puppyList } from "./data";
+import React, { useState } from "react";
+import styles from './App.module.css';
 
 function App() {
+  console.log(puppyList);
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find(puppy => puppy.id === featPupId);
+  console.log(featuredPup);
 
   return (
-
-    console.log(puppyList),
-    <>
-      Puppy Pals
-    </>
-  )
+    <div >
+      <div className={styles['puppy-list']}>
+        <h1>Puppy Pals</h1>
+        <h2>Click on a puppy to see more info</h2>
+        {puppies.map((puppy) => (
+          <p
+            onClick={() => setFeatPupId(puppy.id)}
+            key={puppy.id}
+            className={styles['puppy']}
+          >
+            {puppy.name}
+          </p>
+        ))}
+      </div >
+      {featPupId && (
+        <div className={styles['featured']}>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>
+              <span>Age: {featuredPup.age}</span>
+            </li>
+            <li>
+              <span>Email: {featuredPup.email}</span>
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App 
